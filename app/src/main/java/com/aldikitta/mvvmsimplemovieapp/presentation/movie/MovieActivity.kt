b54ee023fc62e59f6985356ae385b24cab0616aa
Aldikitta <andi.kitta77@gmail.com>
2022-06-03 13:30:35 +0800

    implement use case of movie

--- app/src/main/java/com/aldikitta/mvvmsimplemovieapp/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/aldikitta/mvvmsimplemovieapp/presentation/movie/MovieActivity.kt
@@ -3,6 +3,9 @@ package com.aldikitta.mvvmsimplemovieapp.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -49,6 +52,39 @@ class MovieActivity : AppCompatActivity() {
             } else {
                 binding.movieProgressBar.visibility = View.GONE
                 Toast.makeText(applicationContext, "No Data Available", Toast.LENGTH_LONG).show()
+
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate : MenuInflater = menuInflater
+        inflate.inflate(R.menu.update, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun updateMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
 
             }
         })
